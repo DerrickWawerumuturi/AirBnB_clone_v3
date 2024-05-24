5c6a43d2da4d827e96a952137514bbd22189385a
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-24 14:29:07 +0300

    API_HOST

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,12 +16,12 @@ def call(self):
     storage.close()
 
 if __name__ == '__main__':
-    if getenv('HBNB_API_HOST') is None:
-        HBNB_MYSQL_HOST = '0.0.0.0'
+    if getenv("HBNB_API_HOST") is None:
+        HBNB_API_HOST = '0.0.0.0'
     else:
-        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_API')
-    if getenv('HBNB_MYSQL_PORT') is None:
-        HBNB_MYSQL_PORT = 5000
+        HBNB_API_HOST = getenv("HBNB_API_HOST")
+    if getenv("HBNB_API_PORT") is None:
+        HBNB_API_PORT = 5000
     else:
-        HBNB_MYSQL_PORT = getenv('HBNB_MYSQL_PORT')
-    app.run(host=HBNB_MYSQL_HOST,port=HBNB_MYSQL_PORT, threaded=True)
+        HBNB_API_PORT = int(getenv("HBNB_API_PORT"))
+    app.run(host=HBNB_API_HOST, port=HBNB_API_PORT, threaded=True)
