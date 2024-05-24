028cbf63bc6f66ad230f77d98655d4202fffabeb
Derrick Muturi <wawerumuturi57@gmail.com>
2024-05-24 15:53:08 +0300

    pycodestyle

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,8 @@ Getting the routes
 from api.v1.views import app_views
 from flask import jsonify
 
+
 @app_views.route('/status', methods=['GET'])
 def status():
-   """ routes to status page""" 
-   return jsonify({'status': 'OK'})
+    """ routes to status page"""
+    return jsonify({'status': 'OK'})
