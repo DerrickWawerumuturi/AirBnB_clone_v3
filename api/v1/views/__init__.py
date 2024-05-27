a7e74619feb2a9cc9735ebcd02c41f1b4c8f46a9
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-05-28 00:24:09 +0100

    create a new view for state objects

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,10 +3,7 @@
 
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('/api/v1', __name__, url_prefix="/api/v1")
 
-# routes for Blueprint are defined in api/v1/views/index.py
-try:
-    import api.v1.views.index
-except Exception as e:
-    pass
+from api.v1.views.index import *
+from api.v1.views.states import *
