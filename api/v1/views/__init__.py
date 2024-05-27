31729bf25037e1a37c9b64331c227b49604c20b2
Bendamzi <okunsanyabenjamin40@gmail.com>
2024-05-28 00:08:25 +0100

    task4 correction

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
-"""
-Creating a blueprint
-"""
+"""This modules houses the definition of the views via Blueprint"""
 
 from flask import Blueprint
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
-if app_views is not None:
-    from api.v1.views.index import *
-    from api.v1.views.cities import *
-    from api.v1.views.users import *
-    from api.v1.views.places_reviews import *
-    from api.v1.views.places_amenities import *
+# routes for Blueprint are defined in api/v1/views/index.py
+try:
+    import api.v1.views.index
+except Exception as e:
+    pass
